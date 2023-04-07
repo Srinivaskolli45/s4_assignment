464b1dcd1f9544a39538d959f69a09708097a30f
Srinivaskolli45 <123192396+Srinivaskolli45@users.noreply.github.com>
2023-04-07 15:31:50 +0530

    Update trainModel.py

--- trainModel.py
+++ trainModel.py
@@ -1,32 +1,53 @@
-import torch
 import numpy as np
+import torch
+from tqdm import tqdm
 
 #Training
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def train_model(dataloader,network,lossfn,optimizer,scheduler=None):
-	network.train()
-	train_loss = 0
-	correct = 0
-	total = 0
-	batches = 0
+def train(model, device, train_loader, criterion, scheduler, optimizer, use_l1=False, lambda_l1=0.01):
+   
+    model.train()
+    pbar = tqdm(train_loader)
+    lr_trend = []
+    correct = 0
+    processed = 0
+    train_loss = 0
 
-	for batch_idx, (inputs, targets) in enumerate(dataloader):
-		#inputs = list(inputs.values())
-		#inputs = inputs[0]
-		inputs, targets = inputs.to(device), targets.to(device)
-		optimizer.zero_grad()
-		outputs = network(inputs)
-		loss = lossfn(outputs, targets)
-		loss.backward()
-		optimizer.step()
-		if scheduler != None:
-                 scheduler.step()
-		batches +=1
-		train_loss += loss.item()
-		_, predicted = outputs.max(1)
-		total += targets.size(0)
-		correct += predicted.eq(targets).sum().item()
+    for batch_idx, (data, target) in enumerate(pbar):
+        
+        data, target = data.to(device), target.to(device)
 
-	print('Train model Loss: %.3f | Acc: %.3f%% (%d/%d)'
-					 % (train_loss/(batches), 100.*correct/total, correct, total))
+       
+        optimizer.zero_grad()
+       
+        # Predict
+        y_pred = model(data)
+        # Calculate loss
+        loss = criterion(y_pred, target)
+
+        l1=0
+        if use_l1:
+            for p in model.parameters():
+                l1 = l1 + p.abs().sum()
+        loss = loss + lambda_l1*l1
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        # updating LR
+        if scheduler:
+            if not isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step()
+                lr_trend.append(scheduler.get_last_lr()[0])
+
+        train_loss += loss.item()
+
+        # Update pbar-tqdm
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+        
+
+        pbar.set_description(desc= f'Batch_id={batch_idx} Loss={train_loss/(batch_idx + 1):.5f} Accuracy={100*correct/processed:0.2f}%')
+    return 100*correct/processed, train_loss/(batch_idx + 1), lr_trend
