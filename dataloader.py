2db69daf6822163061f77d2a20864de139876a45
Srinivaskolli45 <123192396+Srinivaskolli45@users.noreply.github.com>
2023-04-07 15:36:57 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,35 @@
+import numpy as np
+from torch.utils.data import Dataset
+import albumentations as A
 import torch
-import torchvision
-import torchvision.transforms as transforms
 
-use_cuda = 'cuda' if torch.cuda.is_available() else 'cpu'
+# custom dataset class for albumentations library
+class AlbumentationImageDataset(Dataset):
+  def __init__(self, image_list, train= True):
+      self.image_list = image_list
+      self.aug = A.Compose({
+          A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+          A.Compose([A.PadIfNeeded(min_height=36, min_width=36, p=1), A.RandomCrop(32, 32)]),
+          A.HorizontalFlip(),
+          A.CoarseDropout(1, 8, 8, 1, 8, 8,fill_value=0.473363, mask_fill_value=None),
+          A.ToGray()
+      })
 
-def getDataLoader(dataset,isTrain,transform,batchSize,isShuffle,workers,needDownload):
-	kwargs = dict(num_workers= workers, pin_memory= True if use_cuda else False,shuffle=isShuffle,batch_size=batchSize)
-	data_set = dataset(root='./data', train=isTrain,
-                                        download=needDownload, transform=transform)
-	dataloader = torch.utils.data.DataLoader(data_set, **kwargs)
-	return dataloader
+      self.norm = A.Compose({A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+      })
+      self.train = train
+        
+  def __len__(self):
+      return (len(self.image_list))
+
+  def __getitem__(self, i):
+      
+      image, label = self.image_list[i]
+      
+      if self.train:
+        #apply augmentation only for training
+        image = self.aug(image=np.array(image))['image']
+      else:
+        image = self.norm(image=np.array(image))['image']
+      image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+      return torch.tensor(image, dtype=torch.float), label
